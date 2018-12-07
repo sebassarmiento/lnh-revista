0c97b0327f5923d4f2daf5b136a3e67114ef5485
Sebastian Sarmiento <sebi_sarmiento@hotmail.com>
2018-12-07 17:00:01 -0300

    Fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,34 +3,35 @@ import '../css/navbar.css';
 import { NavLink } from 'react-router-dom';
 
 class Navbar extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state = {}
+        this.state = { open: false }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         window.addEventListener('scroll', () => this.handleScroll())
     }
 
-    handleScroll(e){
-        if(window.scrollY > 270){
+    handleScroll(e) {
+        if (window.scrollY > 270) {
             console.log(this)
-            this.setState({scrolled: true})
-        } else if(window.scrollX < 270 && this.state.scrolled) {
-            this.setState({scrolled: false})
+            this.setState({ scrolled: true })
+        } else if (window.scrollX < 270 && this.state.scrolled) {
+            this.setState({ scrolled: false })
         }
     }
 
     render() {
         return (
-            <nav className={this.state.scrolled ? 'navbar scrolled' : 'navbar'} >
-                <NavLink to="/" exact >New</NavLink >
-                <NavLink to="/vinos" >Vinos</NavLink >
-                <NavLink to="/gastronomia" >Gastronomia</NavLink >
-                <NavLink to="/turismo" >Turismo</NavLink >
-                <NavLink to="/cultura" >Cultura</NavLink >
-                <NavLink to="/editorial" >Editorial</NavLink >
-                <NavLink to="/contacto" >Contacto</NavLink >
+            <nav onClick={() => this.setState({ open: !this.state.open })} className={this.state.scrolled ? 'navbar scrolled' : this.state.open ? "navbar opened-menu" : 'navbar'} >
+                    <NavLink to="/" exact >New</NavLink >
+                    <NavLink to="/vinos" >Vinos</NavLink >
+                    <NavLink to="/gastronomia" >Gastronomia</NavLink >
+                    <NavLink to="/turismo" >Turismo</NavLink >
+                    <NavLink to="/cultura" >Cultura</NavLink >
+                    <NavLink to="/editorial" >Editorial</NavLink >
+                    <NavLink to="/contacto" >Contacto</NavLink >
+                <div className={this.state.open ? "hamburguer opened" : "hamburguer"} ></div>
             </nav>
         )
     }
